fc01cb25ed5df6b9c9d9a16b1143e2909279823c
afridisaifullah778-lang <afridisaifullah778@gmail.com>
2026-03-10 23:48:42 -0700

    Add files via upload

---
+++ lab 2/lab 2 tasks/lab2 task1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;   // Initialize variable starting from 1
+
+    // while loop runs until i is less than or equal to 10
+    while(i <= 10)
+    {
+        cout << i << endl;   // Display number on separate line
+        i++;                 // Increment value of i
+    }
+
+    return 0;   // End of program
+}
